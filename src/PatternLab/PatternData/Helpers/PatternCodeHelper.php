fb6fce9293628a3aa712af66834c0a311bcfb536
Esteban Solano <esolano@phase2technology.com>
2025-02-27 15:24:36 -0600

    D11 Upgrade
    Code cleanup and fixing deprecated code.

--- src/PatternLab/PatternData/Helpers/PatternCodeHelper.php
+++ src/PatternLab/PatternData/Helpers/PatternCodeHelper.php
@@ -12,6 +12,7 @@
 
 namespace PatternLab\PatternData\Helpers;
 
+use PatternLab\PatternData\Helper;
 use \PatternLab\Config;
 use \PatternLab\Console;
 use \PatternLab\Data;
@@ -21,20 +22,35 @@ use \PatternLab\PatternEngine;
 use \PatternLab\Template;
 use \PatternLab\Timer;
 
-class PatternCodeHelper extends \PatternLab\PatternData\Helper {
-	
+class PatternCodeHelper extends Helper {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $exportFiles;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $exportClean;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $patternPaths;
+
 	public function __construct($options = array()) {
-		
+
 		parent::__construct($options);
-		
+
 		$this->exportFiles  = $options["exportFiles"];
 		$this->exportClean  = $options["exportClean"];
 		$this->patternPaths = $options["patternPaths"];
-		
+
 	}
-	
+
 	public function run() {
-		
+
 		// default vars
 		$options                 = array();
 		$options["patternPaths"] = $this->patternPaths;
@@ -45,22 +61,22 @@ class PatternCodeHelper extends \PatternLab\PatternData\Helper {
 		$patternHead             = Template::getPatternHead();
 		$patternFoot             = Template::getPatternFoot();
 		$stringLoader            = Template::getStringLoader();
-		
+
 		// re-load the pattern data since we modified it
 		$store = PatternData::get();
-		
+
 		// load the pattern loader
 		$patternEngineBasePath   = PatternEngine::getInstance()->getBasePath();
 		$patternLoaderClass      = $patternEngineBasePath."\Loaders\PatternLoader";
 		$patternLoader           = new $patternLoaderClass($options);
-		
+
 		// iterate to process each pattern
 		foreach ($store as $patternStoreKey => $patternStoreData) {
-			
+
 			if (($patternStoreData["category"] == "pattern") && isset($patternStoreData["name"])) {
-				
+
 				$data = Data::getPatternSpecificData($patternStoreKey);
-				
+
 				// add the pattern data so it can be exported
 				$patternData = array();
 				//$patternFooterData["patternFooterData"]["cssEnabled"]      = (Config::$options["enableCSS"] && isset($this->patternCSS[$p])) ? "true" : "false";
@@ -73,32 +89,32 @@ class PatternCodeHelper extends \PatternLab\PatternData\Helper {
 				$patternData["patternName"]       = $patternStoreData["nameClean"];
 				$patternData["patternPartial"]    = $patternStoreData["partial"];
 				$patternData["patternState"]      = $patternStoreData["state"];
-				
+
 				// extra copy for the code view
 				$patternData["extraOutput"]       = isset($patternStoreData["extraOutput"]) ? $patternStoreData["extraOutput"] : array();
-				
+
 				// add the pattern lab specific mark-up
 				// set a default var
 				$exportClean                      = (isset($options["exportClean"])) ? $options["exportClean"] : false;
 				$data["patternLabHead"]           = (!$this->exportFiles) ? $stringLoader->render(array("string" => $htmlHead, "data" => array("cacheBuster" => $data["cacheBuster"]))) : "";
 				$data["patternLabFoot"]           = (!$this->exportFiles) ? $stringLoader->render(array("string" => $htmlFoot, "data" => array("cacheBuster" => $data["cacheBuster"], "isPattern" => true, "patternData" => json_encode($patternData)))) : "";
-				
+
 				if (isset($patternStoreData["patternRaw"])) {
-					
+
 					$header  = (!$this->exportClean) ? $patternLoader->render(array("pattern" => $patternHead, "data" => $data)) : "";
 					$code    = $patternLoader->render(array("pattern" => $patternStoreData["patternRaw"], "data" => $data));
 					$footer  = (!$this->exportClean) ? $patternLoader->render(array("pattern" => $patternFoot, "data" => $data)) : "";
-					
+
 					PatternData::setPatternOption($patternStoreKey,"header",$header);
 					PatternData::setPatternOption($patternStoreKey,"code",$code);
 					PatternData::setPatternOption($patternStoreKey,"footer",$footer);
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 	}
-	
+
 }
